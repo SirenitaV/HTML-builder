350609b6e308580e29240eec845560c97bfe56b5
SirenitaV <sirenita.violetta@gmail.com>
2021-11-01 16:32:04 +0500

    fix: correct solution for 1st task

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -2,8 +2,7 @@ const fs = require('fs');
 const path = require('path');
 const { stdout } = require('process');
 
-fs.readFile(path.join(__dirname, 'text.txt'), 'utf-8', (err, data) => {
-  if (err) throw error;
-  stdout.write(data);
-}
-);
+const stream = fs.createReadStream(path.join(__dirname, 'text.txt'), 'utf-8');
+stream.on('data', text => {
+  stdout.write(text);
+});
