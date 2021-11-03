ee39ef1813143ce41c863e460fc7cace4273e885
SirenitaV <sirenita.violetta@gmail.com>
2021-11-03 17:00:40 +0500

    feat: solution for 2nd task

---
+++ 02-write-file/index.js
@@ -0,0 +1,23 @@
+const fs = require ('fs');
+const path = require ('path');
+const process = require('process');
+
+const stdin = process.stdin;
+const stdout = process.stdout;
+
+stdout.write('Hi! Please, write here anything \n');
+stdin.on('data', (data) => {
+     data.toString();
+     fs.appendFile(path.join(__dirname, 'usertext.txt'), data, (err) => {
+      if(err) throw err;
+   });
+   process.on('SIGINT', () => {
+     console.log('Come again next time! Goodbye!');
+     process.exit();
+   });
+  stdin.on(('data'), (data) => {
+    const exit = 'exit';
+    data = data.toString().trim();
+    data === exit ? (console.log('Come again next time! Goodbye!'), process.exit()) : '';
+  });
+})
