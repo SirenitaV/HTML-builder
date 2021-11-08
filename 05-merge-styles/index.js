08b50efa32d5903a925cc8c6eca862499d299000
SirenitaV <sirenita.violetta@gmail.com>
2021-11-08 17:38:16 +0500

    feat: solution for 5th task

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+const path = require('path');
+
+fs.readdir(path.join(__dirname, 'project-dist'), (err) => {
+  if(err) {
+    console.log(err);
+  }
+   fs.unlink(path.join(__dirname, 'project-dist', 'bundle.css'), (err) => {
+   if (err) throw err;
+   });
+   console.log('deleted bundle.css');
+});
+
+fs.readdir(path.join(__dirname, 'styles'), {withFileTypes: true}, (err, styles) => {
+  if(err) {
+    console.log(err);
+  }
+  else {
+    styles.forEach(style => {
+    let ext = path.extname(path.join(__dirname, 'styles', style.name));
+    if(style.isFile() == true && ext == '.css') {
+      fs.readFile(path.join(__dirname, 'styles', style.name), 'utf-8', (err, styles) => {
+        if(err) {
+          console.log(err);
+        }
+        else {
+          fs.appendFile(path.join(__dirname, 'project-dist', 'bundle.css'), styles, (err, styles) => {
+            if(err) throw err;
+         });
+          console.log('copied to bundle.css');
+        }
+      });
+    }
+  });
+}
+})
