919804ac78d34269decd2201b0c70299cb794f83
SirenitaV <sirenita.violetta@gmail.com>
2021-11-10 22:15:48 +0500

    fix: correct 5th task solution

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,37 +1,39 @@
 const fs = require('fs');
 const path = require('path');
 
-if(!'bundle.css') {
-fs.readdir(path.join(__dirname, 'project-dist'), (err) => {
-  if(err) {
-    console.log(err);
-  }
-   //fs.unlink(path.join(__dirname, 'project-dist', 'bundle.css'), (err) => {
-    fs.writeFile(path.join(__dirname, 'project-dist', 'bundle.css'), (err) => {
-   if (err) throw err;
-   });
-});
-}
-
-fs.readdir(path.join(__dirname, 'styles'), {withFileTypes: true}, (err, styles) => {
-  if(err) {
-    console.log(err);
-  }
-  else {
-    styles.forEach(style => {
-    let ext = path.extname(path.join(__dirname, 'styles', style.name));
-    if(style.isFile() == true && ext == '.css') {
-      fs.readFile(path.join(__dirname, 'styles', style.name), 'utf-8', (err, styles) => {
+    fs.readdir(path.join(__dirname, 'project-dist'), (err) => {
         if(err) {
           console.log(err);
         }
-        else {
-          fs.appendFile(path.join(__dirname, 'project-dist', 'bundle.css'), styles, (err, styles) => {
-            if(err) throw err;
-         });
-        }
-      });
-    }
-  });
-}
-})
+        if(!'bundle.css') {
+          console.log('There is no bundle.css');
+        };
+        if('bundle.css') { 
+            fs.unlink(path.join(__dirname, 'project-dist', 'bundle.css'), () => {
+          if (err) throw err;
+        });     
+       }
+      
+    });
+        fs.readdir(path.join(__dirname, 'styles'), {withFileTypes: true}, (err, styles) => {
+            if(err) {
+              console.log(err);
+            }
+            else {
+              styles.forEach(style => {
+              let ext = path.extname(path.join(__dirname, 'styles', style.name));
+              if(style.isFile() == true && ext == '.css') {
+                fs.readFile(path.join(__dirname, 'styles', style.name), 'utf-8', (err, styles) => {
+                  if(err) {
+                    console.log(err);          
+                  }
+                  else {
+                  fs.appendFile(path.join(__dirname, 'project-dist', 'bundle.css'), styles, (err, styles) => {
+                    if(err) throw err;
+                   });
+                  }
+                });
+              }
+            });
+          }
+          });
