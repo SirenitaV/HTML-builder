1bad4c458826a699515796df85d59ff7f41c0250
SirenitaV <sirenita.violetta@gmail.com>
2021-11-10 15:26:46 +0500

    fix: correct 2nd task

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -4,12 +4,20 @@ const process = require('process');
 
 const stdin = process.stdin;
 const stdout = process.stdout;
+let data = '';
 
-fs.writeFile(path.join(__dirname, 'usertext.txt'), 'utf-8', (err) => {
+fs.writeFile(path.join(__dirname, 'usertext.txt'), data, 'utf-8', (err) => {
   if (err) throw err;
 });
 
 stdout.write('Hi! Please, write here anything \n');
+
+stdin.on(('data'), (data) => {
+    const exit = 'exit';
+    data = data.toString().trim();
+    data === exit ? (console.log('Come again next time! Goodbye!'), process.exit()) : '';
+});
+
 stdin.on('data', (data) => {
      data.toString();
      fs.appendFile(path.join(__dirname, 'usertext.txt'), data, (err) => {
@@ -19,9 +27,9 @@ stdin.on('data', (data) => {
      console.log('Come again next time! Goodbye!');
      process.exit();
    });
-  stdin.on(('data'), (data) => {
-    const exit = 'exit';
-    data = data.toString().trim();
-    data === exit ? (console.log('Come again next time! Goodbye!'), process.exit()) : '';
-  });
+  // stdin.on(('data'), (data) => {
+  //   const exit = 'exit';
+  //   data = data.toString().trim();
+  //   data === exit ? (console.log('Come again next time! Goodbye!'), process.exit()) : '';
+  // });
 })
